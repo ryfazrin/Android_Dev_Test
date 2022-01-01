dc692ffcbdb72f57255fa0c0bff3715ee634582f
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2022-01-01 22:16:54 +0800

    create: model user api

---
+++ lib/model/user_result.dart
@@ -0,0 +1,88 @@
+class UserResult {
+  UserResult({
+    required this.page,
+    required this.perPage,
+    required this.total,
+    required this.totalPages,
+    required this.data,
+    required this.support,
+  });
+
+  int page;
+  int perPage;
+  int total;
+  int totalPages;
+  List<DataUser> data;
+  Support support;
+
+  factory UserResult.fromJson(Map<String, dynamic> json) => UserResult(
+        page: json["page"],
+        perPage: json["per_page"],
+        total: json["total"],
+        totalPages: json["total_pages"],
+        data:
+            List<DataUser>.from(json["data"].map((x) => DataUser.fromJson(x))),
+        support: Support.fromJson(json["support"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "page": page,
+        "per_page": perPage,
+        "total": total,
+        "total_pages": totalPages,
+        "data": List<dynamic>.from(data.map((x) => x.toJson())),
+        "support": support.toJson(),
+      };
+}
+
+class DataUser {
+  DataUser({
+    required this.id,
+    required this.email,
+    required this.firstName,
+    required this.lastName,
+    required this.avatar,
+  });
+
+  int id;
+  String email;
+  String firstName;
+  String lastName;
+  String avatar;
+
+  factory DataUser.fromJson(Map<String, dynamic> json) => DataUser(
+        id: json["id"],
+        email: json["email"],
+        firstName: json["first_name"],
+        lastName: json["last_name"],
+        avatar: json["avatar"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "email": email,
+        "first_name": firstName,
+        "last_name": lastName,
+        "avatar": avatar,
+      };
+}
+
+class Support {
+  Support({
+    required this.url,
+    required this.text,
+  });
+
+  String url;
+  String text;
+
+  factory Support.fromJson(Map<String, dynamic> json) => Support(
+        url: json["url"],
+        text: json["text"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "url": url,
+        "text": text,
+      };
+}
