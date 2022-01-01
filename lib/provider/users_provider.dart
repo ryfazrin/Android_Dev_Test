bf10a87f5ee9ce6ba238cf985ab120bbd7cb99ca
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2022-01-01 22:17:06 +0800

    create: user_provider

---
+++ lib/provider/users_provider.dart
@@ -0,0 +1,46 @@
+import 'package:android_dev_test/api/api_service.dart';
+import 'package:android_dev_test/model/user_result.dart';
+import 'package:flutter/cupertino.dart';
+
+enum ResultState { Loading, NoData, HasData, Error }
+
+class UsersProvider extends ChangeNotifier {
+  late final ApiService apiService;
+
+  UsersProvider({required this.apiService}) {
+    _fetchAllUser();
+  }
+
+  late UserResult _userResult;
+
+  late ResultState _state;
+
+  String _message = '';
+
+  String get message => _message;
+
+  UserResult get result => _userResult;
+
+  ResultState get state => _state;
+
+  Future<dynamic> _fetchAllUser() async {
+    try {
+      _state = ResultState.Loading;
+      notifyListeners();
+      final user = await apiService.getUser();
+      if (user.data.isEmpty) {
+        _state = ResultState.NoData;
+        notifyListeners();
+        return _message = 'Empty User';
+      } else {
+        _state = ResultState.HasData;
+        notifyListeners();
+        return _userResult = user;
+      }
+    } catch (e) {
+      _state = ResultState.Error;
+      notifyListeners();
+      return _message = 'No internet connection';
+    }
+  }
+}
