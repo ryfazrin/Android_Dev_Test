7ded4e8c78e428209b5175e6bcc732a2ab612d41
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2022-01-01 22:17:40 +0800

    update: third_screen, hubungkan provider

--- lib/third_screen.dart
+++ lib/third_screen.dart
@@ -1,5 +1,8 @@
+import 'package:android_dev_test/api/api_service.dart';
+import 'package:android_dev_test/provider/users_provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ThirdScreen extends StatelessWidget {
   const ThirdScreen({Key? key}) : super(key: key);
@@ -13,28 +16,44 @@ class ThirdScreen extends StatelessWidget {
           // style: CupertinoTheme.of(context).textTheme.navTitleTextStyle,
         ),
       ),
-      child: SafeArea(
-        child: CupertinoScrollbar(
-          child: ListView.builder(
-            itemCount: 3,
-            itemBuilder: (BuildContext context, int index) {
-              return CupertinoListTile(
-                leading: Image.asset(
-                  "assets/background.png",
-                  width: 100,
-                  height: 100,
+      child: ChangeNotifierProvider<UsersProvider>(
+        create: (_) => UsersProvider(apiService: ApiService()),
+        child: SafeArea(
+          child: Consumer<UsersProvider>(builder: (context, state, _) {
+            if (state.state == ResultState.Loading) {
+              return Center(child: CircularProgressIndicator());
+            } else if (state.state == ResultState.HasData) {
+              return CupertinoScrollbar(
+                child: ListView.builder(
+                  itemCount: state.result.data.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    var user = state.result.data[index];
+                    return CupertinoListTile(
+                      leading: Image.network(
+                        user.avatar,
+                        width: 100,
+                        height: 100,
+                      ),
+                      title: user.firstName + user.lastName,
+                      subtitle: user.email,
+                      trailing: Text('trailing'),
+                      onTap: () {
+                        // Navigator.push(context, MaterialPageRoute(builder: (context) {
+                        //   return SecondScreen();
+                        // }));
+                      },
+                    );
+                  },
                 ),
-                title: "Test",
-                subtitle: "sub",
-                trailing: Text('trailing'),
-                onTap: () {
-                  // Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  //   return SecondScreen();
-                  // }));
-                },
               );
-            },
-          ),
+            } else if (state.state == ResultState.NoData) {
+              return Center(child: Text(state.message));
+            } else if (state.state == ResultState.Error) {
+              return Center(child: Text(state.message));
+            } else {
+              return Center(child: Text(''));
+            }
+          }),
         ),
       ),
     );
