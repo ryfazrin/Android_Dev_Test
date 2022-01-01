cdba18c42b21b19f8a6df8eb3e46a2531be0c76a
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2022-01-01 21:41:00 +0800

    update: main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:android_dev_test/first_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,87 +12,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
+    return const CupertinoApp(
       title: 'Flutter Demo',
       theme: CupertinoThemeData(
         primaryColor: Color(0xFF2B637B),
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      child: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/background.png"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(33.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset("assets/btn_add_photo.png", width: 116),
-                SizedBox(height: 58.0),
-                CupertinoTextField(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                  ),
-                  placeholder: "Name",
-                  padding: EdgeInsets.only(
-                    top: 7.94,
-                    bottom: 7.94,
-                    left: 20,
-                    right: 16,
-                  ),
-                ),
-                SizedBox(height: 22.0),
-                CupertinoTextField(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                  ),
-                  placeholder: "Polindrome",
-                  padding: EdgeInsets.only(
-                    top: 7.94,
-                    bottom: 7.94,
-                    left: 20,
-                    right: 16,
-                  ),
-                ),
-                SizedBox(height: 45.0),
-                SizedBox(
-                  width: double.infinity,
-                  child: CupertinoButton.filled(
-                    borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                    onPressed: () {},
-                    child: Text("CHECK"),
-                  ),
-                ),
-                SizedBox(height: 15.0),
-                SizedBox(
-                  width: double.infinity,
-                  child: CupertinoButton.filled(
-                    borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                    onPressed: () {},
-                    child: Text("NEXT"),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+      home: FirstScreen(),
     );
   }
 }
